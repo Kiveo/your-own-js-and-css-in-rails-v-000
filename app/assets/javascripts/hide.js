c3de9c76065f2d40d96db4a0cba234610c33752c
Maverick Tesini <mavericktesini@gmail.com>
2018-08-19 07:19:35 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -8,4 +8,4 @@ function hideWhenClicked() {
   // document.getElementById("hide_this").style.display = "none";
 }
 
-$('#hide_this').click(hideWhenClicked);
+$('#hide_this').click(hideWhenClicked());
